9eb1b06098e1dee221d1d87bb940cfe8bd325bca
Michiel Dethmers <michiel-github@tincan.co.uk>
2011-08-24 11:15:17 -0300

    added initial class, tests and some explanation

---
+++ formspamcheck.class.php
@@ -0,0 +1,405 @@
+<?php
+/*
+ * --------------
+ * FormSpamCheck class
+ * Author: Michiel Dethmers, phpList Ltd, http://www.phplist.com
+ * Version 0.1 - 24 August 2011
+ * License: LGPL (Lesser Gnu Public License) http://www.gnu.org/licenses/lgpl-3.0.html
+ * Free to use, distribute and modify in Open as well as Closed Source software
+ * NO WARRANTY WHATSOEVER!
+ * ---------------
+ * 
+ * For more information and how to set up and configure, http://www.phplist.com/formspamclass 
+ *
+ * This class can be used to stop spammers from cluttering your database with bogus signups
+ * posts, comments and whatever else
+ *
+ * It currently uses three services, stopforumspam.com, project honeypot and akismet
+ * If you know of any other services that can be integrated, let me know.
+ * 
+ * 
+ */
+
+class FormSpamCheck {
+
+  private $LE = "\n";
+  private $honeyPotApiKey = '';
+  private $akismetApiKey = '';
+  private $akismetBlogURL = 'http://www.phplist.com';
+  private $memCached = false;
+  private $hpCheck = false;
+  private $akismetCheck = false;
+  private $logRoot = '/var/log';
+  private $logActivity = true;
+  private $UA = 'FormSpamCheck class (v.0.0.1)';
+  // The StopFormSpam API URL
+  private $stopSpamAPIUrl = 'http://www.stopforumspam.com/api';
+  public $matchDetails = '';
+  private $sfsSpamTriggers = array ( ## set a default, in case it's not in config
+    'username' => array ( 
+      'ban_end' => FALSE, 
+      'freq_tolerance' => 2, 
+      'ban_reason' => 'You have been identified as a spammer.',
+    ),
+    'email' => array ( 
+      'ban_end' => FALSE, 
+      'freq_tolerance' => 0,
+      'ban_reason' => 'You have been identified as a spammer.',
+    ),
+    'ip' => array (  
+      'ban_end' => 604800,// 7 days  
+      'freq_tolerance' => 1,
+      'ban_reason' => 'You have been identified as a spammer.',
+    )
+  );
+
+  private $akismetFields = array(
+      'blog',
+      'user_ip',
+      'user_agent',
+      'referrer',
+      'permalink',
+      'comment_type',
+      'comment_author',
+      'comment_author_email',
+      'comment_author_url',
+      'comment_content'
+  );
+
+  function FormSpamCheck() {
+    if (!function_exists('curl_init')) {
+      print 'curl dependency error';
+      return;
+    }
+   # $this->dbg('Init');
+    if (!empty($GLOBALS['honeyPotApiKey'])) {
+      $this->honeyPotApiKey = $GLOBALS['honeyPotApiKey'];
+      $this->hpCheck = true;
+    }
+    if (!empty($GLOBALS['akismetApiKey'])) {
+      $this->akismetApiKey = $GLOBALS['akismetApiKey'];
+     # $this->dbg('Set key '.$GLOBALS['akismetApiKey']);
+      $this->akismetCheck = true;
+    }
+    if (!empty($GLOBALS['akismetBlogURL'])) {
+      $this->akismetBlogURL = $GLOBALS['akismetBlogURL'];
+    }
+    if (!empty($GLOBALS['logRoot']) && is_writable($GLOBALS['logRoot'])) {
+      $this->logRoot = $GLOBALS['logRoot'];
+    }
+    if (isset($GLOBALS['ForumSpamBanTriggers'])) {
+      $this->spamTriggers = $GLOBALS['ForumSpamBanTriggers'];
+    }
+    if (class_exists('Memcached') && isset($GLOBALS['memCachedServer'])) {
+      $this->memCached = new Memcached();
+      if (strpos($GLOBALS['memCachedServer'],':') !== FALSE) {
+        list($server,$port) = explode(':',$GLOBALS['memCachedServer']);
+      } else {
+        $server = $GLOBALS['memCachedServer'];
+        $port = 11211;
+      }
+      $this->memCached->addServer($server,$port);
+    }
+  }
+
+  function dbg($msg) {
+    print $msg."\n";
+  }
+
+  function addLogEntry($logFile,$entry) {
+    if (empty($this->logRoot)) return;
+    if (!$this->logActivity) return;
+    $logFile = basename($logFile,'.log');
+    $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : ' - ';
+    $logEntry = date('Y-m-d H:i:s').' '.$ip.' '.$entry;
+    file_put_contents($this->logRoot.'/'.$logFile.date('Y-m-d').'.log',$logEntry."\n",FILE_APPEND);
+  }
+
+  function getCache($key) {
+    if (!$this->memCached) return false;
+    $val = $this->memCached->get($key);
+ #   print "Cache for $key:  $val\n";
+    return $val;
+  }
+
+  function setCache($key,$val,$expiry = 0) {
+    if (!$this->memCached) return false;
+#    print "Set cache $key = $val\n";
+    if (!$expiry) $expiry = 86400;
+    return $this->memCached->set($key,$val,$expiry);
+  }
+
+  function honeypotCheck($ip) {
+     if (!$this->hpCheck) return;
+
+    ## honeypot requests will be cached in DNS anyway
+    $rev = array_reverse(explode('.', $ip));
+    $lookup = $GLOBALS['honeyPotApiKey'].'.'.implode('.', $rev) . '.dnsbl.httpbl.org';
+
+    $rev = gethostbyname($lookup);
+    $this->addLogEntry('honeypotresult.log',$lookup.' '.$rev);
+    if ($lookup != $rev) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Authenticates your Akismet API key
+  function akismet_verify_key() {
+    if (empty($this->akismetApiKey)) {
+      $this->dbg('No Key');
+      return false;
+    }
+    $cached = $this->getCache('akismetKeyValid');
+    if (!empty($cached)) return $cached;
+    
+    $request = array(
+      'key'=> $this->akismetApiKey,
+      'blog' => $this->akismetBlogURL
+    );
+
+    $keyValid = $this->doPOST('http://rest.akismet.com/1.1/verify-key',$request);
+
+    if ( 'valid' == $keyValid ) {
+      $this->setCache('akismetKeyValid',true);
+      return true;
+    } else {
+      $this->setCache('akismetKeyValid',false);
+      return false;
+    }
+  }
+
+  // Passes back true (it's spam) or false (it's ham)
+  function akismetCheck($data) {
+    if (!$this->akismet_verify_key()) return false;
+
+    ## set some values the way akismet expects them
+    $data['user_ip'] = $data['ips'][0]; ## akismet only handles one IP, so take the first
+    $data['comment_author'] = $data['username'];
+    $data['comment_author_email'] = $data['email'];
+    $data['comment_content'] = $data['content'];
+        
+    foreach ($this->akismetFields as $field) {
+      if (!isset($data[$field])) {
+        switch ($field) {
+          ## set some defaults that will probably return Ham
+          case 'blog': $data['blog'] = $this->akismetBlogURL;break;
+          case 'user_ip': $data['user_ip'] = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR']:'';break;
+          case 'user_agent': $data['user_agent'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT']:'';break;
+          case 'referrer': $data['referrer'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER']:'http://www.wordpress.com';break;
+          case 'permalink': $data['permalink'] = '';break;
+          case 'comment_type': $data['comment_type'] = 'comment';break;
+          case 'comment_author': $data['comment_author'] = 'Admin';break;
+          case 'comment_author_email': $data['comment_author_email'] = 'formspamcheck@gmail.com';break;
+          case 'comment_author_url': $data['comment_author_url'] = '';break;
+          case 'comment_content': $data['comment_content'] = '';break;
+        }
+      }
+    }
+
+    $cached = $this->getCache('akismet'.md5(serialize($data)));
+    if (!empty($cached)) {
+      $isSpam = $cached;
+    } else {
+      $isSpam = $this->doPOST('http://'.$this->akismetApiKey.'.rest.akismet.com/1.1/comment-check',$data);
+      $this->setCache('akismet'.md5(serialize($data)),$isSpam);
+    }
+    if ( 'true' == $isSpam ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function doPOST($url,$requestdata = array()) {
+    $date = date('r');
+    
+    $requestheader = array(
+      'Host: '.parse_url($url,PHP_URL_HOST),
+      'Content-Type: application/x-www-form-urlencoded',
+      'Date: '. $date,
+    );
+    $data = '';
+    foreach ($requestdata as $param => $value) {
+      if (!is_array($value)) {
+        $data .= $param.'='.urlencode($value).'&';
+      } // else -> forget about arrays for now
+    }
+    $data = substr($data,0,-1);
+    $requestheader[] = 'Content-Length: '.strlen($data);
+     
+    $header = '';
+    foreach ($requestheader as $param) {
+      $header .= $param.$this->LE;
+    }
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); 
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($curl, CURLOPT_HTTPHEADER,$requestheader);
+    curl_setopt($curl, CURLOPT_DNS_USE_GLOBAL_CACHE, TRUE); 
+    curl_setopt($curl, CURLOPT_USERAGENT,$this->UA);
+    curl_setopt($curl, CURLOPT_POST, 1);
+
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        
+    $result = curl_exec($curl);
+    $status = curl_getinfo($curl,CURLINFO_HTTP_CODE);
+    if ($status != 200) {
+      $error = curl_error($curl);
+      $this->dbg('Curl Error '.$status.' '.$error);
+    }
+    curl_close($curl);
+    return $result;
+  }
+
+  function doGET($cUrl) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $cUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    return $result;
+  }
+
+  function stopForumSpamCheck($data = array()) {
+
+#  function checkForSpam($user_name = '',$user_ips = array(),$user_email = '') {
+    if (!sizeof($data['ips'])) {
+      $data['ips'][] = $_SERVER['REMOTE_ADDR'];
+      if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $data['ips'][] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+      }
+    }
+    
+    $isSfsSpam = false;
+    $logSpamPath = $_SERVER['DOCUMENT_ROOT'].'/store/spammers'.date('Y-m-d').'.log';
+    $logHamPath = $_SERVER['DOCUMENT_ROOT'].'/store/nonspammers'.date('Y-m-d').'.log';
+    $logAPICallPath = $_SERVER['DOCUMENT_ROOT'].'/store/ban-apicall'.date('Y-m-d').'.log';
+
+    $spamTriggers = $this->sfsSpamTriggers;
+    if (empty($data['username'])) {
+      unset($spamTriggers['username']);
+    } else {
+      $spamTriggers['username']['value'] = $data['username'];
+    }
+    if (empty($data['ips'])) {
+      unset($spamTriggers['ip']);
+    } else {
+      $spamTriggers['ip']['value'] = $data['ips'];
+    }
+    if (empty($data['email'])) {
+      unset($spamTriggers['email']);
+    } else {
+      $spamTriggers['email']['value'] = $data['email'];
+    }
+
+    $apiRequest = '';
+    foreach ($spamTriggers as $trigger => $banDetails) {
+      if (!empty($banDetails['value'])) {
+        if (is_array($banDetails['value'])) {
+          foreach ($banDetails['value'] as $v) {
+            $apiRequest .= $trigger.'[]='.$v.'&';
+          }
+        } else {
+          $apiRequest .= $trigger.'[]='.$banDetails['value'].'&';
+        }
+      }
+    }
+
+    $cached = $this->getCache('SFS'.$apiRequest);
+    if (!$cached) {
+      $cUrl = $this->stopSpamAPIUrl.'?'.$apiRequest.'&unix';
+      $this->addLogEntry('sfs-apicall.log',$cUrl);
+      $xml = $this->doGET($cUrl);
+          
+      if (!$xml) {
+        $this->addLogEntry('sfs-apicall.log','FAIL ON XML');
+        return false;
+      }
+      $this->setCache('SFS'.$apiRequest,$xml);
+    } else {
+      $xml = $cached;
+    }
+    ## the resulting XML is an 
+    $response = simplexml_load_string($xml);
+    
+  #  var_dump($response);exit;
+    $spamMatched = array();
+    if ($response->success) {
+      foreach ($spamTriggers as $trigger => $banDetails) {
+        ## iterate over the results found, eg email, ip and username
+        foreach ($response->$trigger as $resultEntry) {
+          if ($resultEntry->appears) {
+         #   var_dump($resultEntry);
+            if (!empty($banDetails['ban_end']) && $resultEntry->lastseen+$banDetails['ban_end'] > time()) {
+              $isSfsSpam = true;
+              $banDetails['matchedon'] = $trigger;
+              $banDetails['matchedvalue'] = (string)$resultEntry->value;
+              $banDetails['frequency'] = (string)$resultEntry->frequency;
+            }
+            if ((int)$resultEntry->frequency > $banDetails['freq_tolerance']) {
+              $isSfsSpam = true;
+              $banDetails['matchedon'] = $trigger;
+              $banDetails['matchedvalue'] = (string)$resultEntry->value;
+              $banDetails['frequency'] = (string)$resultEntry->frequency;
+            }
+          }
+          $spamMatched[] = $banDetails;
+        }
+      }
+    }
+    # var_dump($spamMatched);
+    $this->matchDetails = $spamMatched;
+    return $isSfsSpam;
+  }
+
+} // eo class
+
+
+/*
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+  return;
+}
+*/
+
+// Change to FALSE to disable the mod.
+if (FALSE) {
+  // Only execute this if not already identified as banned.
+  if (!$banned && empty($row['ban_exclude'])) {
+    global $phpbb_root_path;
+    require_once($phpbb_root_path.'includes/functions_user.php');
+
+    $spamCheck = new ForumSpamCheck();
+    #$_SERVER['REMOTE_ADDR'];
+    
+
+    // Unset any checks that we don't have values for.
+    if (empty($user_id)) {
+      unset($banTriggers['username']);
+    } elseif (isset($banTriggers['username'])) {
+      user_get_id_name($user_id, $user_name);
+      if (!empty($user_name) && $user_name[1] != 'Anonymous') {
+        $banTriggers['username']['value'] = $user_name;
+      } else {
+        unset($banTriggers['username']);
+      }
+    }
+    if (empty($user_email)) {
+      unset($banTriggers['email']);
+    } elseif (isset($banTriggers['email'])) {
+      $banTriggers['email']['value'] = $user_email;
+    }
+    if (empty($user_ips)) {
+      unset($banTriggers['ip']);
+    } elseif (isset($banTriggers['ip'])) {
+      $banTriggers['ip']['value'] = $_SERVER['REMOTE_ADDR'];
+    }
+
+
+  }
+}
+?>
---
+++ tests.php
@@ -0,0 +1,72 @@
+<?php
+
+## some tests on the formspamcheck class
+
+require dirname(__FILE__).'/../config.php';
+require 'formspamcheck.class.php';
+
+## fetch some obvious spam from eg http://programmermeetdesigner.com/blog/view/techstars_ceo__project_posting_and_the_pmd_difference_/#comments
+
+$spam = array (
+  'ips' => array('92.100.15.254','91.121.55.3'),
+  'user_agent' => 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.2.20) Gecko/20110803 Firefox/3.6.20',
+  'referrer' => 'http://wordpress.com',
+  'username' => 'zusecon',
+  'email' => 'l.ihai.y.an.0.01@gmail.com',
+  'content' => 'c3cC0T <a href="http://www.postrocknotes.com/acomplia.html">acomplia no script pay master card</a> 445312 <a href="http://www.postrocknotes.com/phentermine.html">overnight phentermine</a> tmioor <a href="http://www.100japanesethings.com/valium.html">valium</a> :) <a href="http://www.100japanesethings.com/xanax.html">but xanax online overnight by fedex</a> 715 <a href="http://www.sincerebro.net/acomplia.html">acomplia</a> =-DD <a href="http://www.haroldlopezgarroz.com/phentermine.html">phentermine</a> %-)))',
+);
+
+$ham = array (
+  'ips' => array('77.240.14.92'),
+  'user_agent' => 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.2.20) Gecko/20110803 Firefox/3.6.20',
+  'referrer' => 'http://wordpress.com',
+  'username' => 'HelloWorld',
+  'email' => 'phplist@gmail.com',
+  'content' => '  First, our friend over at TechStars, David Cohen, responsible for such Web 2.0 companies as J-Squared Media and madKast.com, just let me know he posted a new project over here. Mr. Cohen is a seasoned expert and has lead many rounds of venture funding for his companies. He\'s hoping to find a "RockStar" programmer to put a new project together. Turning to PMD because of its unique userbase of highly qualified, cutting edge, and entrepreneurial motivated users.
+
+Many of you will remember our article about IntenseDebate, the Web 2.0 startup, started by three guys who similarly met on PMD. They, each having different skill sets, came together to contribute to the idea that eventually became IntenseDebate. We\'ve all heard of this story before: A "2.0" company starts, with a small team of motivated programmers, designers and/or writers working in someone\'s basement. What makes this story unique is that the three founding members were located all over the world. Now that they\'ve secured funding (in large part due to TechStars), I\'m sure they\'ve traded in their Skype and AIM for inter-office shouting.
+
+I think this story helps illustrate the difference between PMD and all of the other outsourcing/freelancing/job-boards on the Internet. In addition to having the most highly skilled user base around, ProgrammerMeetDesigner has built a community of people looking to create serious and lasting Internet companies in a cooperative fashion. For the most part, when people meet on PMD, they\'re not looking to scam each other, get one over on the other person, or rip anyone off. At least in my experience/observation I\'ve seen a great deal of synergy created when people come together to work on PMD, and if that\'s not unique in today\'s day and age of outsourcing, I don\'t know what is.',
+
+);
+
+$fsc = new FormSpamCheck();
+
+print "SFS\n";
+if ($fsc->stopForumSpamCheck($spam)) {
+  print "SPAM\n";
+} else {
+  print "HAM\n";
+}
+#var_dump($fsc->matchDetails);
+
+if ($fsc->stopForumSpamCheck($ham)) {
+  print "SPAM\n";
+} else {
+  print "HAM\n";
+}
+#var_dump($fsc->matchDetails);
+
+print 'Akismet'."\n";
+if ($fsc->akismetCheck($spam)) {
+  print "SPAM\n";
+} else {
+  print "HAM\n";
+}
+if ($fsc->akismetCheck($ham)) {
+  print "SPAM\n";
+} else {
+  print "HAM\n";
+}
+print 'Honeypot'."\n";
+if ($fsc->honeypotcheck($spam['ips'][0])) {
+  print "SPAM\n";
+} else {
+  print "HAM\n";
+}
+if ($fsc->honeypotcheck($ham['ips'][0])) {
+  print "SPAM\n";
+} else {
+  print "HAM\n";
+}
+
