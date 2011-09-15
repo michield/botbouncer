7fdfefc9605f1f69f86c12d3de998e84a698ebfb
Michiel Dethmers <michiel-github@tincan.co.uk>
2011-09-15 14:17:10 -0300

    add mollom example

--- example.php
+++ example.php
@@ -3,10 +3,12 @@
 $honeypotApiKey = 'My API key for Honeypot Project';
 $akismetApiKey  = 'My API key for Akismet';
 $akismetBlogURL = 'http://www.example.com';
+$mollomPublicKey = 'mollomPubKey';
+$mollomPrivateKey = 'mollomPrivKey';
 
 include 'formspamcheck.class.php';
 
-$fsc = new FormSpamCheck($honeypotApiKey,$akismetApiKey,$akismetBlogURL);
+$fsc = new FormSpamCheck($honeypotApiKey,$akismetApiKey,$akismetBlogURL,$mollomPrivateKey,$mollomPublicKey);
 if ($fsc->isSpam(
   array(
     'username' => 'someusername',
